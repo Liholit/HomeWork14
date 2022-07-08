d04b6bbc6c2aec7772684d726eaa088e5ca0c1dc
Igor Liholit <igorliholit2335@gmail.com>
2022-07-08 21:21:51 +0300

    commit

--- src/Bet.java
+++ src/Bet.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Bet {
+
+    public static void main(String[] args) {
+        Bet bet = new Bet();
+
+    }
+
+    public Bet() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введіть вашу ставку");
+        double stavka = scan.nextDouble();
+        System.out.println("Введіть ваш ризик");
+        double risk = scan.nextDouble();
+        System.out.println("Ваша ставка " + stavka + " грн" + "\nРизик програшу " + risk + " %");
+        System.out.println();
+    }
 }
